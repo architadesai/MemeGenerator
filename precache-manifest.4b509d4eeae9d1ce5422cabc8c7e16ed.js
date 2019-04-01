f5c130010e696d28558217660ce3f1aeee9d2690
Archita Desai <architad8@gmail.com>
2019-04-01 19:24:42 +0530

    Updates

---
+++ precache-manifest.4b509d4eeae9d1ce5422cabc8c7e16ed.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "deaab6e92b58a61d2df3",
+    "url": "/MemeGenerator/static/js/runtime~main.6e92fdf5.js"
+  },
+  {
+    "revision": "d2eb5e4bf8912f49703e",
+    "url": "/MemeGenerator/static/js/main.78bf1118.chunk.js"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/MemeGenerator/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "83243f5a52fefa6c5a00e840404ed1cc",
+    "url": "/MemeGenerator/index.html"
+  }
+];
